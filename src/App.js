30658a0f8d590508e1613d852101a01cf74a7808
MarkHamiltonII <markhamiltonii@gmail.com>
2023-09-20 23:46:15 -0500

    Adding react app, python parser, and start of github pages

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import seating_chart from './data/seating_chart.json'
+
+
+function App() {
+  const [seatingList] = useState(seating_chart)
+  const [query, setQuery] = useState("")
+  
+  const filteredList = seatingList.filter(item=> {
+    return item.Name.toLowerCase().includes(query.toLowerCase())
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to Heather and Sarah's Wedding
+        </h1>
+        <p>
+          Find your seat below
+        </p>
+      </header>
+      <div className='search-bar'>
+        <input type='text' className='search-query' placeholder='Find your name' onChange={e => setQuery(e.target.value)} />
+        <button className='search-button'>
+          <FaSearch />
+        </button>
+      </div>
+      <div className='seating-chart'>
+        <div className='seating-labels'><p>Name</p><p>Table #</p></div>
+        {filteredList.map(person => {
+          return (
+            <div className='person'><p>{person.Name}</p><p>{person.Table}</p></div>)
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/parse_seating.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+
+df = pd.read_excel('src\data\seating.xlsx', index_col=None)
+
+seating_chart = []
+
+for row in df.index:
+    person = {}
+    for column in df:
+        person[column] = str(df[column][row])
+    seating_chart.append(person)
+
+with open("src/data/seating_chart.json", "w") as outfile:
+    json.dump(seating_chart, outfile)
