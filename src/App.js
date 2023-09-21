84c200fa2cc7da857294755ba0c8db9c7f843b2d
MarkHamiltonII <markhamiltonii@gmail.com>
2023-09-21 00:20:43 -0500

    Added search by table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
 import seating_chart from './data/seating_chart.json'
 
 
 function App() {
   const [seatingList] = useState(seating_chart)
   const [query, setQuery] = useState("")
-  
-  const filteredList = seatingList.filter(item=> {
-    return item.Name.toLowerCase().includes(query.toLowerCase())
-  })
+  const [tableNumber, setTableNumber] = useState()
+
+  let filteredList = seatingList
+
+  if (query) {
+    filteredList = seatingList.filter(item => {
+      return item.Name.toLowerCase().includes(query.toLowerCase())
+    })
+  }
+
+  if (tableNumber) {
+    filteredList = seatingList.filter(item => {
+      return item.Table.toLowerCase().includes(tableNumber.toLowerCase())
+    })
+  }
 
   return (
     <div className="App">
@@ -23,10 +33,24 @@ function App() {
         </p>
       </header>
       <div className='search-bar'>
-        <input type='text' className='search-query' placeholder='Search by name' onChange={e => setQuery(e.target.value)} />
-        <button className='search-button'>
-          <FaSearch />
-        </button>
+        <input 
+        type='text' 
+        value={query}
+        className='search' 
+        placeholder='Search by name' 
+        onChange={e => {
+          setQuery(e.target.value)
+          setTableNumber("")
+          }} />
+        <input 
+        type='text' 
+        value={tableNumber}
+        className='search' 
+        placeholder='Search by table' 
+        onChange={e => {
+          setTableNumber(e.target.value)
+          setQuery("")
+          }} />
       </div>
       <div className='seating-chart'>
         <div className='seating-labels'><p>Name</p><p>Table #</p></div>
